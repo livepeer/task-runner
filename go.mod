555c14b800d08a7d9d87659fbf1fca250db06c72
Victor Elias <victorgelias@gmail.com>
2022-01-31 20:21:56 -0300

    task: Implement file upload logic on import task

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.16
 
 require (
 	github.com/golang/glog v1.0.0
-	github.com/livepeer/go-api-client v0.0.0-20220126222908-cf886f606b02 // indirect
+	github.com/livepeer/go-api-client v0.0.0-20220126222908-cf886f606b02
+	github.com/livepeer/go-livepeer v0.5.26
 	github.com/livepeer/livepeer-data v0.4.8
 	github.com/peterbourgon/ff v1.7.1
 	github.com/rabbitmq/amqp091-go v1.1.0
@@ -12,5 +13,6 @@ require (
 
 replace (
 	github.com/livepeer/go-api-client => ../go-api-client
+	github.com/livepeer/go-livepeer => ../go-livepeer
 	github.com/livepeer/livepeer-data => ../livepeer-data
 )
--- task/import.go
+++ task/import.go
@@ -3,18 +3,34 @@ package task
 import (
 	"io/ioutil"
 	"net/http"
+	"path"
+	"time"
 
 	"github.com/golang/glog"
+	"github.com/livepeer/go-livepeer/drivers"
 )
+
+const fileUploadTimeout = 5 * time.Minute
 
 func TaskImport(tctx *TaskContext) error {
 	var (
 		ctx    = tctx.Context
 		srcURL = tctx.Params.Import.URL
+		osURL  = tctx.ObjectStore.URL
+		// TODO: Re-evaluate this file path. Maybe omit the user ID and/or use a playbackID instead of asset ID (primary key)?
+		filePath = path.Join(tctx.Asset.UserID, tctx.Asset.ID, "video.mp4")
 	)
+
+	osDriver, err := drivers.ParseOSURL(osURL, true)
+	if err != nil {
+		glog.Errorf("Error parsing object store url=%s err=%q", osURL, err)
+		return nil
+	}
+	osSess := osDriver.NewSession("")
+
 	req, err := http.NewRequestWithContext(ctx, "GET", srcURL, nil)
 	if err != nil {
-		glog.Errorf("Error creating http request err=%q", err)
+		glog.Errorf("Error creating http request url=%s err=%q", srcURL, err)
 		return nil
 	}
 	resp, err := http.DefaultClient.Do(req)
@@ -22,15 +38,24 @@ func TaskImport(tctx *TaskContext) error {
 		glog.Errorf("Error downloading task input err=%v", err)
 		return nil
 	} else if resp.StatusCode >= 300 {
-		glog.Errorf("Status error downloading task input. status=%d", resp.StatusCode)
+		glog.Errorf("Status error downloading task input status=%d", resp.StatusCode)
 		return nil
 	}
 
-	// Here be fantasy
-	body, err := ioutil.ReadAll(resp.Body)
+	// TODO: ObjectStore driver must receive a Reader instead of the entire file buffered in memory.
+	contents, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		glog.Errorf("Error reading file from remote URL err=%q", err)
 		return err
 	}
-	glog.Info("Import task success. response=%q", string(body))
+
+	_, err = osSess.SaveData(ctx, filePath, contents, nil, fileUploadTimeout)
+	if err != nil {
+		glog.Errorf("Error uploading file to object store filePath=%q err=%q", filePath, err)
+		return err
+	}
+
+	// TODO: Register success on the API
+	glog.Info("Import task success! filePath=%q", filePath)
 	return nil
 }
