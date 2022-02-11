071d23ce473372af07485a2b6b44d321dd113f02
Victor Elias <victorgelias@gmail.com>
2022-02-11 20:33:01 -0300

    task: Check progress and report more often
    Check every second and report anytime it passes
    0.25, 0.5 or 0.75, so we're not so silent for
    smaller files

--- task/progress.go
+++ task/progress.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"math"
 	"runtime/debug"
+	"sort"
 	"time"
 
 	"github.com/golang/glog"
 	livepeerAPI "github.com/livepeer/go-api-client"
 )
 
-const progressReportInterval = 10 * time.Second
+var progressReportBuckets = []float64{0, 0.25, 0.5, 0.75, 1}
+
+const minProgressReportInterval = 10 * time.Second
+const progressCheckInterval = 1 * time.Second
 
 func ReportProgress(ctx context.Context, lapi *livepeerAPI.Client, taskID string, size uint64, getCount func() uint64) {
 	defer func() {
@@ -21,19 +25,38 @@ func ReportProgress(ctx context.Context, lapi *livepeerAPI.Client, taskID string
 	if size <= 0 {
 		return
 	}
-	timer := time.NewTicker(progressReportInterval)
+	var (
+		timer        = time.NewTicker(progressCheckInterval)
+		lastProgress = float64(0)
+		lastReport   time.Time
+	)
 	defer timer.Stop()
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case <-timer.C:
-			progress := float64(getCount()) / float64(size)
-			progress = math.Round(progress*1000) / 1000
-			progress = math.Min(progress, 0.99)
+			progress := calcProgress(getCount(), size)
+			if time.Since(lastReport) < minProgressReportInterval &&
+				progressBucket(progress) == progressBucket(lastProgress) {
+				continue
+			}
 			if err := lapi.UpdateTaskStatus(taskID, "running", progress); err != nil {
 				glog.Errorf("Error updating task progress taskID=%s progress=%v err=%q", taskID, progress, err)
+				continue
 			}
+			lastReport, lastProgress = time.Now(), progress
 		}
 	}
+}
+
+func calcProgress(count, size uint64) (val float64) {
+	val = float64(count) / float64(size)
+	val = math.Round(val*1000) / 1000
+	val = math.Min(val, 0.99)
+	return
+}
+
+func progressBucket(progress float64) int {
+	return sort.SearchFloat64s(progressReportBuckets, progress)
 }
