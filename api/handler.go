5c4e0fd3201eba77e1644519eaffc56dcbd1a61c
Victor Elias <victorgelias@gmail.com>
2022-08-04 19:05:08 -0300

    api: Create dummy catalyst hook API
    While at it, also copy prometheus stuff from the
    analyzer and set it up as well.

--- api/handler.go
+++ api/handler.go
@@ -2,9 +2,12 @@ package api
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"path"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/livepeer/task-runner/metrics"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -26,9 +29,22 @@ func NewHandler(serverCtx context.Context, opts APIHandlerOptions) http.Handler 
 	if opts.Prometheus {
 		router.Handler("GET", "/metrics", promhttp.Handler())
 	}
+	router.Handler("POST", CataylistHookPath(opts.APIRoot, ":id"),
+		metrics.ObservedHandlerFunc("catalyst_hook", handler.catalystHook))
 	return router
 }
 
 func (h *apiHandler) healthcheck(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusOK)
+}
+
+func (h *apiHandler) catalystHook(rw http.ResponseWriter, r *http.Request) {
+	taskId := httprouter.ParamsFromContext(r.Context()).ByName("id")
+	// TODO: something
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("task " + taskId))
+}
+
+func CataylistHookPath(apiRoot, taskId string) string {
+	return path.Join(apiRoot, fmt.Sprintf("/webhook/catalyst/task/%s", taskId))
 }
