5b34dafa92131cc521023553c76a86a95d4a6a3a
Victor Elias <victorgelias@gmail.com>
2022-08-05 18:19:42 -0300

    task: Create upload task

---
+++ task/upload.go
@@ -0,0 +1,11 @@
+package task
+
+import (
+	"errors"
+
+	"github.com/livepeer/livepeer-data/pkg/data"
+)
+
+func TaskUpload(tctx *TaskContext) (*data.TaskOutput, error) {
+	return nil, errors.New("not implemented")
+}
