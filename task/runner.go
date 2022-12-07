66ef87d7135321eb4fafc8e667a6db092fa77c46
Victor Elias <victorgelias@gmail.com>
2022-12-07 20:37:37 -0300

    task/runner: Several reliability improvements  (#95)
    * go.mod: Update go-api-client for all retries
    
    * runner: Handle rate limit errors from API and delay task
    
    * runner: Hide catalyst task lost error as well
    
    * runner: Fix log
    
    * go.mod: Update livepeer-data for amqp reliability
    
    * task: Handle panics more gracefully
    
    Move it to the outer function in runner so we
    catch errors in the runner code as well
    
    * task: Make sure we publish all events with a separate timeout
    
    we dont want to not sent the publish result because we already
    expired the task contexst or something.
    
    * task: Setup dead-lettering for main tasks queue
    
    * go.mod: Update to merged livepeer-data
    
    * Fix test
    
    * OMG add some spacing man
    
    * Stop double wrapping an error
    
    * task: Add migration logic to new queue
    
    * cmd: Make the default value of dead letter empty
    
    Just so the deploys can be safe
    
    * runner: Move cleanup logic to it's own function
    
    * task: Make all AMQP publishes mandatory
    
    * task: Add TODO about handling AMQP returns
    
    * task: Handle some mediaconvert errors
    
    * task: Disable mandatory message if we don't handle them
    
    * runner: Remove some forgotten unused args

--- task/runner.go
+++ task/runner.go
@@ -23,14 +23,19 @@ const (
 	DefaultMaxTaskProcessingTime = 10 * time.Minute
 	DefaultMinTaskProcessingTime = 5 * time.Second
 	DefaultMaxConcurrentTasks    = 3
+	taskPublishTimeout           = 1 * time.Minute
 )
 
-var defaultTasks = map[string]TaskHandler{
-	"import":    TaskImport,
-	"upload":    TaskUpload,
-	"export":    TaskExport,
-	"transcode": TaskTranscode,
-}
+var (
+	defaultTasks = map[string]TaskHandler{
+		"import":    TaskImport,
+		"upload":    TaskUpload,
+		"export":    TaskExport,
+		"transcode": TaskTranscode,
+	}
+	errInternalProcessingError = errors.New("internal error processing file")
+	taskFatalErrorInfo         = &data.ErrorInfo{Message: errInternalProcessingError.Error(), Unretriable: true}
+)
 
 type TaskHandlerOutput struct {
 	*data.TaskOutput
@@ -71,6 +76,10 @@ type Runner interface {
 type RunnerOptions struct {
 	AMQPUri                 string
 	ExchangeName, QueueName string
+	OldQueueName            string
+	DeadLetter              struct {
+		ExchangeName, QueueName string
+	}
 
 	MinTaskProcessingTime time.Duration
 	MaxTaskProcessingTime time.Duration
@@ -97,6 +106,9 @@ func NewRunner(opts RunnerOptions) Runner {
 	}
 	if opts.MaxConcurrentTasks == 0 {
 		opts.MaxConcurrentTasks = DefaultMaxConcurrentTasks
+	}
+	if opts.DeadLetter.ExchangeName != "" && opts.DeadLetter.QueueName == "" {
+		opts.DeadLetter.QueueName = opts.DeadLetter.ExchangeName
 	}
 	return &runner{
 		RunnerOptions:   opts,
@@ -134,48 +146,88 @@ func (r *runner) Start() error {
 		return fmt.Errorf("error consuming queue: %w", err)
 	}
 
+	// TODO: Remove this logic after migration to dead leterred queue
+	if r.OldQueueName != "" {
+		go cleanUpOldQueue(r.AMQPUri, r.OldQueueName, r.ExchangeName)
+	}
+
 	r.amqp = amqp
 	return nil
 }
 
 func (r *runner) setupAmqpConnection(c event.AMQPChanSetup) error {
-	err := c.ExchangeDeclare(r.ExchangeName, "topic", true, false, false, false, nil)
-	if err != nil {
-		return fmt.Errorf("error ensuring API exchange exists: %w", err)
-	}
-	_, err = c.QueueDeclare(r.QueueName, true, false, false, false, amqp.Table{"x-queue-type": "quorum"})
-	if err != nil {
-		return fmt.Errorf("error declaring task queue: %w", err)
-	}
-	err = c.QueueBind(r.QueueName, "task.trigger.#", r.ExchangeName, false, nil)
-	if err != nil {
-		return fmt.Errorf("error binding task queue: %w", err)
+	queueArgs := amqp.Table{"x-queue-type": "quorum"}
+	if dlx := r.DeadLetter; dlx.ExchangeName != "" {
+		err := declareQueueAndExchange(c, dlx.ExchangeName, dlx.QueueName, "#", queueArgs)
+		if err != nil {
+			return err
+		}
+		queueArgs["x-dead-letter-exchange"] = r.DeadLetter.ExchangeName
 	}
 
-	err = c.ExchangeDeclare(r.DelayedExchange, "topic", true, false, false, false, nil)
+	err := declareQueueAndExchange(c, r.ExchangeName, r.QueueName, "task.trigger.#", queueArgs)
 	if err != nil {
-		return fmt.Errorf("error declaring delayed exchange: %w", err)
+		return err
 	}
-	delayedArgs := amqp.Table{
+
+	queueArgs = amqp.Table{
 		"x-message-ttl":          int32(time.Minute / time.Millisecond),
 		"x-dead-letter-exchange": r.ExchangeName,
 	}
-	_, err = c.QueueDeclare(r.DelayedExchange, true, false, false, false, delayedArgs)
+	err = declareQueueAndExchange(c, r.DelayedExchange, r.DelayedExchange, "#", queueArgs)
 	if err != nil {
-		return fmt.Errorf("error declaring delayed queue: %w", err)
+		return err
 	}
-	err = c.QueueBind(r.DelayedExchange, "#", r.DelayedExchange, false, nil)
-	if err != nil {
-		return fmt.Errorf("error binding delayed queue: %w", err)
-	}
-	err = c.Qos(int(r.MaxConcurrentTasks), 0, false)
-	if err != nil {
+
+	if err := c.Qos(int(r.MaxConcurrentTasks), 0, false); err != nil {
 		return fmt.Errorf("error setting QoS: %w", err)
 	}
 	return nil
 }
 
-func (r *runner) handleAMQPMessage(msg amqp.Delivery) error {
+func cleanUpOldQueue(amqpURI, oldQueue, exchange string) {
+	defer func() {
+		if r := recover(); r != nil {
+			glog.Errorf("Recovered from panic in cleanUpOldQueue: %v", r)
+		}
+	}()
+
+	cleanUp := event.NewAMQPConnectFunc(func(c event.AMQPChanSetup) error {
+		err := c.QueueUnbind(oldQueue, "task.trigger.#", oldQueue, nil)
+		if err != nil {
+			glog.Errorf("Error unbinding old queue from exchange queue=%q exchange=%q err=%q", oldQueue, exchange, err)
+		}
+
+		_, err = c.QueueDelete(oldQueue, false, true, false)
+		if err != nil {
+			glog.Errorf("Error deleting old queue=%q err=%q", oldQueue, err)
+		}
+
+		return nil
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	cleanUp(ctx, amqpURI, nil, nil)
+}
+
+func declareQueueAndExchange(c event.AMQPChanSetup, exchange, queue, binding string, queueArgs amqp.Table) error {
+	err := c.ExchangeDeclare(exchange, "topic", true, false, false, false, nil)
+	if err != nil {
+		return fmt.Errorf("error declaring exchange %q: %w", exchange, err)
+	}
+	_, err = c.QueueDeclare(queue, true, false, false, false, queueArgs)
+	if err != nil {
+		return fmt.Errorf("error declaring queue %q: %w", queue, err)
+	}
+	err = c.QueueBind(queue, binding, exchange, false, nil)
+	if err != nil {
+		return fmt.Errorf("error binding queue %q on %q to exchange %q: %w", queue, binding, exchange, err)
+	}
+	return nil
+}
+
+func (r *runner) handleAMQPMessage(msg amqp.Delivery) (err error) {
 	// rate-limit message processing time to limit load
 	defer blockUntil(time.After(r.MinTaskProcessingTime))
 
@@ -185,8 +237,14 @@ func (r *runner) handleAMQPMessage(msg amqp.Delivery) error {
 	task, err := parseTaskInfo(msg)
 	if err != nil {
 		glog.Errorf("Error parsing AMQP message err=%q msg=%q", err, msg.Body)
-		return nil
+		return event.UnprocessableMsgErr(err)
 	}
+	defer func() {
+		if rec := recover(); rec != nil {
+			glog.Errorf("Panic handling task type=%s id=%s step=%q panic=%v stack=%q", task.Type, task.ID, task.Step, rec, debug.Stack())
+			err = simplePublishTaskFatalError(r.amqp, r.ExchangeName, task)
+		}
+	}()
 
 	output, err := r.handleTask(ctx, task)
 	if err == nil && output != nil && output.Continue {
@@ -196,20 +254,11 @@ func (r *runner) handleAMQPMessage(msg amqp.Delivery) error {
 
 	glog.Infof("Task handler processed task type=%q id=%s output=%+v error=%q unretriable=%v", task.Type, task.ID, output, err, IsUnretriable(err))
 
-	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Minute)
-	defer cancel()
 	// return the error directly so that if publishing the result fails we nack the message to try again
-	return r.publishTaskResult(ctx, task, output, err)
+	return r.publishTaskResult(task, output, err)
 }
 
 func (r *runner) handleTask(ctx context.Context, taskInfo data.TaskInfo) (out *TaskHandlerOutput, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			glog.Errorf("Panic handling task: value=%q stack:\n%s", r, string(debug.Stack()))
-			err = UnretriableError{fmt.Errorf("panic handling task: %v", r)}
-		}
-	}()
-
 	taskCtx, err := r.buildTaskContext(ctx, taskInfo)
 	if err != nil {
 		return nil, fmt.Errorf("error building task context: %w", err)
@@ -231,7 +280,10 @@ func (r *runner) handleTask(ctx context.Context, taskInfo data.TaskInfo) (out *T
 		}
 
 		err = r.lapi.UpdateTaskStatus(taskID, api.TaskPhaseRunning, 0)
-		if err != nil {
+		if err == api.ErrRateLimited {
+			glog.Warningf("Task execution rate limited type=%q id=%s userID=%s", taskType, taskID, taskCtx.UserID)
+			return nil, r.delayTaskStep(ctx, taskID, taskCtx.Step, taskCtx.StepInput)
+		} else if err != nil {
 			glog.Errorf("Error updating task progress type=%q id=%s err=%q unretriable=%v", taskType, taskID, err, IsUnretriable(err))
 			// execute the task anyway
 		}
@@ -255,7 +307,7 @@ func parseTaskInfo(msg amqp.Delivery) (data.TaskInfo, error) {
 }
 
 func (r *runner) buildTaskContext(ctx context.Context, info data.TaskInfo) (*TaskContext, error) {
-	task, err := r.lapi.GetTask(info.ID)
+	task, err := r.lapi.GetTask(info.ID, true)
 	if err != nil {
 		return nil, err
 	}
@@ -275,7 +327,7 @@ func (r *runner) getAssetAndOS(assetID string) (*api.Asset, *api.ObjectStore, dr
 	if assetID == "" {
 		return nil, nil, nil, nil
 	}
-	asset, err := r.lapi.GetAsset(assetID)
+	asset, err := r.lapi.GetAsset(assetID, true)
 	if err != nil {
 		return nil, nil, nil, err
 	}
@@ -296,8 +348,10 @@ func (r *runner) HandleCatalysis(ctx context.Context, taskId, nextStep, attemptI
 	if err != nil {
 		return fmt.Errorf("failed to get task %s: %w", taskId, err)
 	}
+
 	glog.Infof("Received catalyst callback taskType=%q id=%s taskPhase=%s status=%q completionRatio=%v error=%q rawCallback=%+v",
 		task.Type, task.ID, task.Status.Phase, callback.Status, callback.CompletionRatio, callback.Error, *callback)
+
 	if task.Status.Phase != api.TaskPhaseRunning &&
 		task.Status.Phase != api.TaskPhaseWaiting {
 		return fmt.Errorf("task %s is not running", taskId)
@@ -305,6 +359,7 @@ func (r *runner) HandleCatalysis(ctx context.Context, taskId, nextStep, attemptI
 		return fmt.Errorf("outdated catalyst job callback, "+
 			"task has already been retried (callback: %s current: %s)", attemptID, curr)
 	}
+
 	progress := 0.9 * callback.CompletionRatio
 	progress = math.Round(progress*1000) / 1000
 	currProgress, taskUpdatedAt := task.Status.Progress, data.NewUnixMillisTime(task.Status.UpdatedAt)
@@ -314,13 +369,15 @@ func (r *runner) HandleCatalysis(ctx context.Context, taskId, nextStep, attemptI
 			glog.Warningf("Failed to update task progress. taskID=%s err=%v", task.ID, err)
 		}
 	}
+
 	if callback.Status == clients.CatalystStatusError {
 		glog.Infof("Catalyst job failed for task type=%q id=%s error=%q unretriable=%v", task.Type, task.ID, callback.Error, callback.Unretriable)
 		err := NewCatalystError(callback.Error, callback.Unretriable)
-		return r.publishTaskResult(ctx, taskInfo, nil, err)
+		return r.publishTaskResult(taskInfo, nil, err)
 	} else if callback.Status == clients.CatalystStatusSuccess {
-		return r.scheduleTaskStep(ctx, task.ID, nextStep, callback)
+		return r.scheduleTaskStep(task.ID, nextStep, callback)
 	}
+
 	return nil
 }
 
@@ -332,12 +389,12 @@ func (r *runner) delayTaskStep(ctx context.Context, taskID, step string, input i
 	if err != nil {
 		return err
 	}
-	return r.publishLogged(ctx, task, r.DelayedExchange,
+	return r.publishLogged(task, r.DelayedExchange,
 		fmt.Sprintf("task.trigger.%s", task.Type),
 		data.NewTaskTriggerEvent(task))
 }
 
-func (r *runner) scheduleTaskStep(ctx context.Context, taskID, step string, input interface{}) error {
+func (r *runner) scheduleTaskStep(taskID, step string, input interface{}) error {
 	if step == "" {
 		return errors.New("can only schedule sub-steps of tasks")
 	}
@@ -346,13 +403,13 @@ func (r *runner) scheduleTaskStep(ctx context.Context, taskID, step string, inpu
 		return err
 	}
 	key, body := fmt.Sprintf("task.trigger.%s", task.Type), data.NewTaskTriggerEvent(task)
-	if err := r.publishLogged(ctx, task, r.ExchangeName, key, body); err != nil {
+	if err := r.publishLogged(task, r.ExchangeName, key, body); err != nil {
 		return fmt.Errorf("error publishing task result event: %w", err)
 	}
 	return nil
 }
 
-func (r *runner) publishTaskResult(ctx context.Context, task data.TaskInfo, output *TaskHandlerOutput, resultErr error) error {
+func (r *runner) publishTaskResult(task data.TaskInfo, output *TaskHandlerOutput, resultErr error) error {
 	if r.HumanizeErrors {
 		resultErr = humanizeError(resultErr)
 	}
@@ -365,14 +422,14 @@ func (r *runner) publishTaskResult(ctx context.Context, task data.TaskInfo, outp
 		return errors.New("output or resultErr must be non-nil")
 	}
 	key := fmt.Sprintf("task.result.%s.%s", task.Type, task.ID)
-	if err := r.publishLogged(ctx, task, r.ExchangeName, key, body); err != nil {
+	if err := r.publishLogged(task, r.ExchangeName, key, body); err != nil {
 		return fmt.Errorf("error publishing task result event: %w", err)
 	}
 	return nil
 }
 
 func (r *runner) getTaskInfo(id, step string, input interface{}) (data.TaskInfo, *api.Task, error) {
-	task, err := r.lapi.GetTask(id)
+	task, err := r.lapi.GetTask(id, true)
 	if err != nil {
 		return data.TaskInfo{}, nil, fmt.Errorf("error getting task %q: %w", id, err)
 	}
@@ -396,20 +453,8 @@ func (r *runner) getTaskInfo(id, step string, input interface{}) (data.TaskInfo,
 	}, task, nil
 }
 
-func (r *runner) publishLogged(ctx context.Context, task data.TaskInfo, exchange, key string, body interface{}) error {
-	msg := event.AMQPMessage{
-		Exchange:   exchange,
-		Key:        key,
-		Body:       body,
-		Persistent: true,
-		WaitResult: true,
-	}
-	glog.Infof("Publishing AMQP message. taskType=%q id=%s step=%q exchange=%q key=%q body=%+v", task.Type, task.ID, task.Step, exchange, key, body)
-	if err := r.amqp.Publish(ctx, msg); err != nil {
-		glog.Errorf("Error publishing AMQP message. taskType=%q id=%s step=%q exchange=%q key=%q err=%q body=%+v", task.Type, task.ID, task.Step, exchange, key, err, body)
-		return err
-	}
-	return nil
+func (r *runner) publishLogged(task data.TaskInfo, exchange, key string, body interface{}) error {
+	return publishLoggedRaw(r.amqp, task, exchange, key, body)
 }
 
 func (r *runner) Shutdown(ctx context.Context) error {
@@ -436,14 +481,18 @@ func humanizeError(err error) error {
 	if errors.As(err, &catErr) {
 		if strings.Contains(errMsg, "unsupported input pixel format") {
 			return errors.New("unsupported input pixel format, must be 'yuv420p' or 'yuvj420p'")
+		} else if strings.Contains(errMsg, "Unsupported video input") {
+			return errors.New("unsupported file format")
+		} else if strings.Contains(errMsg, "ReadPacketData File read failed - end of file hit") {
+			return errors.New("invalid video file, possibly truncated")
 		}
-		return errors.New("internal error processing file")
+		return errInternalProcessingError
 	}
 
 	if strings.Contains(errMsg, "unexpected eof") {
 		return errors.New("file download failed")
 	} else if strings.Contains(errMsg, "multipartupload: upload multipart failed") {
-		return errors.New("internal error saving file to storage")
+		return errors.New("error saving file to storage")
 	} else if strings.Contains(errMsg, "mp4io: parse error") {
 		return UnretriableError{errors.New("file format unsupported, must be a valid MP4")}
 	}
@@ -452,10 +501,11 @@ func humanizeError(err error) error {
 		strings.Contains(errMsg, "could not create stream id") ||
 		strings.Contains(errMsg, "502 bad gateway") ||
 		strings.Contains(errMsg, "task has already been started before") ||
+		strings.Contains(errMsg, "catalyst task lost") ||
 		(strings.Contains(errMsg, "eof") && strings.Contains(errMsg, "error processing file"))
 
 	if isProcessing {
-		return errors.New("internal error processing file")
+		return errInternalProcessingError
 	}
 
 	isTimeout := strings.Contains(errMsg, "context deadline exceeded") ||
@@ -470,3 +520,37 @@ func humanizeError(err error) error {
 }
 
 func blockUntil(t <-chan time.Time) { <-t }
+
+// This is a code path to send a task failure event the simplest way possible,
+// to be used when handling panics in the task processing code path. It is
+// purposedly meant to be a separate flow as much as possible to avoid any
+// chance of hitting the same panic again.
+func simplePublishTaskFatalError(producer event.AMQPProducer, exchange string, task data.TaskInfo) error {
+	body := data.NewTaskResultEvent(task, taskFatalErrorInfo, nil)
+	key := taskResultMessageKey(task.Type, task.ID)
+	return publishLoggedRaw(producer, task, exchange, key, body)
+}
+
+func publishLoggedRaw(producer event.AMQPProducer, task data.TaskInfo, exchange, key string, body interface{}) error {
+	ctx, cancel := context.WithTimeout(context.Background(), taskPublishTimeout)
+	defer cancel()
+	msg := event.AMQPMessage{
+		Exchange:   exchange,
+		Key:        key,
+		Body:       body,
+		Persistent: true,
+		// TODO: Actually handle returns from the AMQP server so we can toggle mandatory here. Needs further support in pkg/event.
+		// Mandatory:  true,
+		WaitResult: true,
+	}
+	glog.Infof("Publishing AMQP message. taskType=%q id=%s step=%q exchange=%q key=%q body=%+v", task.Type, task.ID, task.Step, exchange, key, body)
+	if err := producer.Publish(ctx, msg); err != nil {
+		glog.Errorf("Error publishing AMQP message. taskType=%q id=%s step=%q exchange=%q key=%q err=%q body=%+v", task.Type, task.ID, task.Step, exchange, key, err, body)
+		return err
+	}
+	return nil
+}
+
+func taskResultMessageKey(ttype, id string) string {
+	return fmt.Sprintf("task.result.%s.%s", ttype, id)
+}
