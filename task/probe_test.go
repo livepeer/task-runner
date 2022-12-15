8ff0f5669e3b3109acb1eccee5540ebaa9fa7fcd
Yondon Fu <yondon.fu@gmail.com>
2022-12-15 12:42:11 -0500

    task/probe: Add unit test for parseFps
    Add testify pkg for testing.

---
+++ task/probe_test.go
@@ -0,0 +1,51 @@
+package task
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseFps(t *testing.T) {
+	assert := assert.New(t)
+
+	// single value, invalid
+	_, err := parseFps("foobar")
+	assert.ErrorContains(err, "error parsing framerate:")
+
+	// single value
+	fps, err := parseFps("5")
+	assert.Nil(err)
+	assert.Equal(fps, 5.0)
+
+	// fraction, invalid
+	_, err = parseFps("foo/bar")
+	assert.ErrorContains(err, "error parsing framerate numerator:")
+
+	// invalid numerator
+	_, err = parseFps("foo/1")
+	assert.ErrorContains(err, "error parsing framerate numerator:")
+
+	// invalid denominator
+	_, err = parseFps("1/foo")
+	assert.ErrorContains(err, "error parsing framerate denominator:")
+
+	// 1/0
+	// -1/0
+	// 0/0
+
+	// 5/1
+	fps, err = parseFps("5/1")
+	assert.Nil(err)
+	assert.Equal(fps, 5.0)
+
+	// 1/1
+	fps, err = parseFps("1/1")
+	assert.Nil(err)
+	assert.Equal(fps, 1.0)
+
+	// 1/4
+	fps, err = parseFps("1/4")
+	assert.Nil(err)
+	assert.Equal(fps, 0.25)
+}
