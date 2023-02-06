52f41f06f33eec2f491ab2a4ea1393b66351340d
Victor Elias <victorgelias@gmail.com>
2023-02-06 18:37:49 -0300

    task/encryption: Handle PKCS#7 padding properly

--- task/encryption/decrypt.go
+++ task/encryption/decrypt.go
@@ -1,11 +1,14 @@
 package encryption
 
 import (
+	"bufio"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/hex"
 	"fmt"
 	"io"
+
+	"github.com/d1str0/pkcs7"
 )
 
 func DecryptAES256CBCReader(reader io.ReadCloser, keyb16 string) (io.ReadCloser, error) {
@@ -24,7 +27,7 @@ func DecryptAES256CBCReader(reader io.ReadCloser, keyb16 string) (io.ReadCloser,
 		return nil, fmt.Errorf("error reading iv from input: %w", err)
 	}
 
-	stream := cipher.NewCBCDecrypter(block, iv)
+	decrypter := cipher.NewCBCDecrypter(block, iv)
 	pipeReader, pipeWriter := io.Pipe()
 
 	go func() {
@@ -37,21 +40,39 @@ func DecryptAES256CBCReader(reader io.ReadCloser, keyb16 string) (io.ReadCloser,
 		}()
 
 		buffer := make([]byte, 256*aes.BlockSize)
+		bufReader := bufio.NewReaderSize(reader, 2*len(buffer))
 		for {
-			n, err := io.ReadFull(reader, buffer)
+			n, err := io.ReadFull(bufReader, buffer)
 			if n == 0 || err == io.EOF {
 				break
 			} else if err != nil && err != io.ErrUnexpectedEOF {
+				// unexpected EOF is returned when input ends before the buffer size
+				pipeWriter.CloseWithError(err)
+				return
+			} else if n%aes.BlockSize != 0 {
+				// this can only ever happen on the last chunk
+				err := fmt.Errorf("input is not a multiple of AES block size. must be padded with PKCS#7")
 				pipeWriter.CloseWithError(err)
 				return
 			}
 
-			// padding only happens on the last block and we just ignore the extra bytes
-			padSize := (aes.BlockSize - (n % aes.BlockSize)) % aes.BlockSize
-			paddedSize := n + padSize
-			stream.CryptBlocks(buffer[:paddedSize], buffer[:paddedSize])
+			chunk := buffer[:n]
+			decrypter.CryptBlocks(chunk, chunk)
 
-			if _, err := pipeWriter.Write(buffer[:n]); err != nil {
+			if _, peekErr := bufReader.Peek(1); peekErr == io.EOF {
+				// this means we're on the last chunk, so handle padding
+				lastBlock := chunk[len(chunk)-aes.BlockSize:]
+				unpadded, err := pkcs7.Unpad(lastBlock)
+				if err != nil {
+					pipeWriter.CloseWithError(fmt.Errorf("bad input PKCS#7 padding: %w", err))
+					return
+				}
+
+				padSize := len(lastBlock) - len(unpadded)
+				chunk = chunk[:len(chunk)-padSize]
+			}
+
+			if _, err := pipeWriter.Write(chunk); err != nil {
 				pipeWriter.CloseWithError(err)
 				return
 			}
