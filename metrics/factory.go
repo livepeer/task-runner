04e88e7a54122b1b314fc3434df7b5c7981c1502
Victor Elias <victorgelias@gmail.com>
2023-05-04 12:08:49 -0300

    Revert "Revert "metrics: Use custom registry to avoid metric conflicts""
    This reverts commit 0016b52fefc3dce8b4bffe4ab8b571f7b83f2b7e.

--- metrics/factory.go
+++ metrics/factory.go
@@ -1,16 +1,28 @@
 package metrics
 
 import (
+	"net/http"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
 	Namespace = "livepeer"
 	Subsystem = "task_runner"
-	Factory   = promauto.With(prometheus.DefaultRegisterer)
+	// We use catalyst-api as a library which uses prometheus lib and default
+	// registry. Avoid metric conflicts by using a custom registry.
+	Registry = prometheus.NewRegistry()
+	Factory  = promauto.With(Registry)
 )
 
 func FQName(name string) string {
 	return prometheus.BuildFQName(Namespace, Subsystem, name)
+}
+
+func ScrapeHandler() http.Handler {
+	handler := promhttp.HandlerFor(Registry, promhttp.HandlerOpts{})
+	handler = promhttp.InstrumentMetricHandler(Registry, handler)
+	return handler
 }
