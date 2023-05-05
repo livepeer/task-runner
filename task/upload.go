761bc5348d06253b39c0fb1e806cc9215176d534
Rafał Leszko <rafal@livepeer.org>
2023-05-05 10:01:17 +0200

    Generate always MP4 output for recordings

--- task/upload.go
+++ task/upload.go
@@ -553,7 +553,13 @@ func removeCredentials(metadata *clients.CatalystCallback) *clients.CatalystCall
 func uploadTaskOutputLocations(tctx *TaskContext) ([]OutputName, []clients.OutputLocation, error) {
 	playbackId := tctx.OutputAsset.PlaybackID
 	outURL := tctx.OutputOSObj.URL
-	outputNames, outputLocations, err := outputLocations(outURL, OUTPUT_ENABLED, playbackId, OUTPUT_ONLY_SHORT, playbackId)
+	var mp4 string
+	if tctx.OutputAsset.Source.Type == "recording" {
+		mp4 = OUTPUT_ENABLED
+	} else {
+		mp4 = OUTPUT_ONLY_SHORT
+	}
+	outputNames, outputLocations, err := outputLocations(outURL, OUTPUT_ENABLED, playbackId, mp4, playbackId)
 	if err != nil {
 		return nil, nil, err
 	}
